cf3a787ced4f86bc775df73ca573040e7d8512b4
OmarInCS <Omar.Abdelkarim@tuwaiq.edu.sa>
2022-02-24 11:14:12 +0300

    Initial Commit

---
+++ MapAndGallery/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  MapAndGallery
+//
+//  Created by user on 2/24/22.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    var locationMgr = CLLocationManager()
+    var userLoc: CLLocationCoordinate2D!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let center = CLLocationCoordinate2D(latitude: 24.7933648, longitude: 46.7674239)
+        let region = MKCoordinateRegion(center: center, span: span)
+        map.setRegion(region, animated: true)
+        
+        let marker = MKPointAnnotation()
+        marker.coordinate = center
+        marker.title = "Atiaf Mall"
+        marker.subtitle = "A nice Mall"
+        
+        map.addAnnotation(marker)
+        
+        locationMgr.desiredAccuracy = kCLLocationAccuracyBest
+        locationMgr.requestWhenInUseAuthorization()
+        locationMgr.startUpdatingLocation()
+        locationMgr.delegate = self
+    }
+
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        userLoc = locations.first?.coordinate
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: userLoc, span: span)
+        map.setRegion(region, animated: true)
+        
+        map.removeAnnotations(map.annotations)
+        
+        let marker = MKPointAnnotation()
+        marker.coordinate = userLoc
+        marker.title = "User Location"
+        
+        map.addAnnotation(marker)
+        
+        
+    }
+
+}
+
